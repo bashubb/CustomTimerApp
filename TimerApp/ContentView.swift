c9335460ea1b0fe0b26718406163cd6781507f0b
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-06-27 11:45:26 +0200

    Initial Commit

---
+++ TimerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimerApp
+//
+//  Created by HubertMac on 26/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+ 
+    
+    var body: some View {
+        Text("hello world")
+    }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimerApp/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  TimerApp
+//
+//  Created by HubertMac on 26/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
